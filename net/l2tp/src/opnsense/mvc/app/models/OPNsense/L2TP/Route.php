b2ad4d61ada3b2988afe8799267c5b0e68834ff2
Sergey Kirpa <Kirpa.Sergey@gmail.com>
2019-04-18 16:21:36 +0300

    Fix Gateway editing instead of selecting from the list
    (cherry picked from commit 93d0126d57edeba5d804499acf7a6d1bac9dd984)

--- net/l2tp/src/opnsense/mvc/app/models/OPNsense/L2TP/Route.php
+++ net/l2tp/src/opnsense/mvc/app/models/OPNsense/L2TP/Route.php
@@ -53,15 +53,12 @@ class Route extends BaseModel
                 // Use specified message from regular validators to report issues back to the user
                 if (in_array($node->getInternalXMLTagName(), array("gateway", "network"))) {
                     $route = $node->getParentNode();
-                    $proto_net = strpos($route->network, ':') === false ? "inet" : "inet6";
-                    // Gateway addresses are stored in the result list received from configd.
-                    // Unfortunately we can't trust the config here, so we use the list results here.
-                    $gateway = $route->gateway->getNodeData()[(string)$route->gateway];
-                    $tmp = explode("-", $gateway['value']);
-                    $gateway_ip = !empty($tmp) ? end($tmp) : "";
-                    $gateway_proto = strpos($gateway_ip, ":") !== false ? "inet6" : "inet";
+                    $net_proto = strpos($route->network, ':') === false ? "inet" : "inet6";
+                    $gateway_ip = (string) $route->gateway;
+                    $gateway_proto = strpos($gateway_ip, ":") === false ? "inet" : "inet6";
+
                     // When protocols don't match, add a message for this field to the validation result.
-                    if (empty($gateway_ip) || $gateway_proto != $proto_net) {
+                    if (empty($gateway_ip) || $gateway_proto != $net_proto) {
                         $node_validators = $node->getValidators();
                         $result->appendMessage(new \Phalcon\Validation\Message(
                             $node_validators[0]->getOption("message"),
